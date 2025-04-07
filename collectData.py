7e91e8fed2f5251410fe7763b930c220801ba167
asoderlind <axesode@gmail.com>
2025-04-07 10:12:08 +0200

    auto format and vehicle toggle'

--- collectData.py
+++ collectData.py
@@ -14,12 +14,18 @@ import sys
 import time
 
 try:
-    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
-        sys.version_info.major,
-        sys.version_info.minor,
-        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+    sys.path.append(
+        glob.glob(
+            "../carla/dist/carla-*%d.%d-%s.egg"
+            % (
+                sys.version_info.major,
+                sys.version_info.minor,
+                "win-amd64" if os.name == "nt" else "linux-x86_64",
+            )
+        )[0]
+    )
 except IndexError:
-    print('carla not found')
+    print("carla not found")
     pass
 
 import carla
@@ -33,14 +39,16 @@ from matplotlib import pyplot as plt
 import cv2
 import carla_vehicle_annotator as cva
 
+
 def retrieve_data(sensor_queue, frame, timeout=5):
     while True:
         try:
-            data = sensor_queue.get(True,timeout)
+            data = sensor_queue.get(True, timeout)
         except queue.Empty:
             return None
         if data.frame == frame:
             return data
+
 
 save_rgb = True
 save_depth = False
@@ -48,49 +56,53 @@ save_segm = False
 save_lidar = False
 tick_sensor = 1
 
-def main():
 
-    argparser = argparse.ArgumentParser(
-        description=__doc__)
+def main():
+    argparser = argparse.ArgumentParser(description=__doc__)
     argparser.add_argument(
-        '--host',
-        metavar='H',
-        default='127.0.0.1',
-        help='IP of the host server (default: 127.0.0.1)')
+        "--host",
+        metavar="H",
+        default="127.0.0.1",
+        help="IP of the host server (default: 127.0.0.1)",
+    )
     argparser.add_argument(
-        '-p', '--port',
-        metavar='P',
+        "-p",
+        "--port",
+        metavar="P",
         default=2000,
         type=int,
-        help='TCP port to listen to (default: 2000)')
+        help="TCP port to listen to (default: 2000)",
+    )
     argparser.add_argument(
-        '-n', '--number-of-vehicles',
-        metavar='N',
+        "-n",
+        "--number-of-vehicles",
+        metavar="N",
         default=50,
         type=int,
-        help='number of vehicles (default: 10)')
+        help="number of vehicles (default: 10)",
+    )
     argparser.add_argument(
-        '-tm_p', '--tm_port',
-        metavar='P',
+        "-tm_p",
+        "--tm_port",
+        metavar="P",
         default=8000,
         type=int,
-        help='port to communicate with TM (default: 8000)')
+        help="port to communicate with TM (default: 8000)",
+    )
 
     args = argparser.parse_args()
-    
+
     vehicles_list = []
     nonvehicles_list = []
     client = carla.Client(args.host, args.port)
     client.set_timeout(10.0)
 
     try:
-
         traffic_manager = client.get_trafficmanager(args.tm_port)
         traffic_manager.set_global_distance_to_leading_vehicle(2.0)
         world = client.get_world()
 
- 
-        print('\nRUNNING in synchronous mode\n')
+        print("\nRUNNING in synchronous mode\n")
         settings = world.get_settings()
         traffic_manager.set_synchronous_mode(True)
         if not settings.synchronous_mode:
@@ -101,7 +113,7 @@ def main():
         else:
             synchronous_master = False
 
-        blueprints = world.get_blueprint_library().filter('vehicle.*')
+        blueprints = world.get_blueprint_library().filter("vehicle.*")
 
         spawn_points = world.get_map().get_spawn_points()
         number_of_spawn_points = len(spawn_points)
@@ -109,7 +121,7 @@ def main():
         if args.number_of_vehicles < number_of_spawn_points:
             random.shuffle(spawn_points)
         elif args.number_of_vehicles > number_of_spawn_points:
-            msg = 'Requested %d vehicles, but could only find %d spawn points'
+            msg = "Requested %d vehicles, but could only find %d spawn points"
             logging.warning(msg, args.number_of_vehicles, number_of_spawn_points)
             args.number_of_vehicles = number_of_spawn_points
 
@@ -125,14 +137,20 @@ def main():
             if n >= args.number_of_vehicles:
                 break
             blueprint = random.choice(blueprints)
-            if blueprint.has_attribute('color'):
-                color = random.choice(blueprint.get_attribute('color').recommended_values)
-                blueprint.set_attribute('color', color)
-            if blueprint.has_attribute('driver_id'):
-                driver_id = random.choice(blueprint.get_attribute('driver_id').recommended_values)
-                blueprint.set_attribute('driver_id', driver_id)
-            blueprint.set_attribute('role_name', 'autopilot')
-            batch.append(SpawnActor(blueprint, transform).then(SetAutopilot(FutureActor, True)))
+            if blueprint.has_attribute("color"):
+                color = random.choice(
+                    blueprint.get_attribute("color").recommended_values
+                )
+                blueprint.set_attribute("color", color)
+            if blueprint.has_attribute("driver_id"):
+                driver_id = random.choice(
+                    blueprint.get_attribute("driver_id").recommended_values
+                )
+                blueprint.set_attribute("driver_id", driver_id)
+            blueprint.set_attribute("role_name", "autopilot")
+            batch.append(
+                SpawnActor(blueprint, transform).then(SetAutopilot(FutureActor, True))
+            )
             spawn_points.pop(0)
 
         for response in client.apply_batch_sync(batch, synchronous_master):
@@ -141,19 +159,19 @@ def main():
             else:
                 vehicles_list.append(response.actor_id)
 
-        print('Created %d npc vehicles \n' % len(vehicles_list))
-        
+        print("Created %d npc vehicles \n" % len(vehicles_list))
+
         # -----------------------------
         # Spawn ego vehicle and sensors
         # -----------------------------
         q_list = []
         idx = 0
-        
+
         tick_queue = queue.Queue()
         world.on_tick(tick_queue.put)
         q_list.append(tick_queue)
         tick_idx = idx
-        idx = idx+1
+        idx = idx + 1
 
         # Spawn ego vehicle
         ego_bp = random.choice(blueprints)
@@ -161,24 +179,24 @@ def main():
         ego_vehicle = world.spawn_actor(ego_bp, ego_transform)
         vehicles_list.append(ego_vehicle)
         ego_vehicle.set_autopilot(True)
-        print('Ego-vehicle ready')
+        print("Ego-vehicle ready")
 
         # Spawn RGB camera
         cam_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
-        cam_bp = world.get_blueprint_library().find('sensor.camera.rgb')
-        cam_bp.set_attribute('sensor_tick', str(tick_sensor))
+        cam_bp = world.get_blueprint_library().find("sensor.camera.rgb")
+        cam_bp.set_attribute("sensor_tick", str(tick_sensor))
         cam = world.spawn_actor(cam_bp, cam_transform, attach_to=ego_vehicle)
         nonvehicles_list.append(cam)
         cam_queue = queue.Queue()
         cam.listen(cam_queue.put)
         q_list.append(cam_queue)
         cam_idx = idx
-        idx = idx+1
-        print('RGB camera ready')
+        idx = idx + 1
+        print("RGB camera ready")
 
         # Spawn depth camera
-        depth_bp = world.get_blueprint_library().find('sensor.camera.depth')
-        depth_bp.set_attribute('sensor_tick', str(tick_sensor))
+        depth_bp = world.get_blueprint_library().find("sensor.camera.depth")
+        depth_bp.set_attribute("sensor_tick", str(tick_sensor))
         depth = world.spawn_actor(depth_bp, cam_transform, attach_to=ego_vehicle)
         cc_depth_log = carla.ColorConverter.LogarithmicDepth
         nonvehicles_list.append(depth)
@@ -186,13 +204,15 @@ def main():
         depth.listen(depth_queue.put)
         q_list.append(depth_queue)
         depth_idx = idx
-        idx = idx+1
-        print('Depth camera ready')
+        idx = idx + 1
+        print("Depth camera ready")
 
         # Spawn segmentation camera
         if save_segm:
-            segm_bp = world.get_blueprint_library().find('sensor.camera.semantic_segmentation')
-            segm_bp.set_attribute('sensor_tick', str(tick_sensor))
+            segm_bp = world.get_blueprint_library().find(
+                "sensor.camera.semantic_segmentation"
+            )
+            segm_bp.set_attribute("sensor_tick", str(tick_sensor))
             segm_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
             segm = world.spawn_actor(segm_bp, segm_transform, attach_to=ego_vehicle)
             cc_segm = carla.ColorConverter.CityScapesPalette
@@ -201,18 +221,18 @@ def main():
             segm.listen(segm_queue.put)
             q_list.append(segm_queue)
             segm_idx = idx
-            idx = idx+1
-            print('Segmentation camera ready')
+            idx = idx + 1
+            print("Segmentation camera ready")
 
         # Spawn LIDAR sensor
         if save_lidar:
-            lidar_bp = world.get_blueprint_library().find('sensor.lidar.ray_cast')
-            lidar_bp.set_attribute('sensor_tick', str(tick_sensor))
-            lidar_bp.set_attribute('channels', '64')
-            lidar_bp.set_attribute('points_per_second', '1120000')
-            lidar_bp.set_attribute('upper_fov', '30')
-            lidar_bp.set_attribute('range', '100')
-            lidar_bp.set_attribute('rotation_frequency', '20')
+            lidar_bp = world.get_blueprint_library().find("sensor.lidar.ray_cast")
+            lidar_bp.set_attribute("sensor_tick", str(tick_sensor))
+            lidar_bp.set_attribute("channels", "64")
+            lidar_bp.set_attribute("points_per_second", "1120000")
+            lidar_bp.set_attribute("upper_fov", "30")
+            lidar_bp.set_attribute("range", "100")
+            lidar_bp.set_attribute("rotation_frequency", "20")
             lidar_transform = carla.Transform(carla.Location(x=0, z=4.0))
             lidar = world.spawn_actor(lidar_bp, lidar_transform, attach_to=ego_vehicle)
             nonvehicles_list.append(lidar)
@@ -220,8 +240,8 @@ def main():
             lidar.listen(lidar_queue.put)
             q_list.append(lidar_queue)
             lidar_idx = idx
-            idx = idx+1
-            print('LIDAR ready')
+            idx = idx + 1
+            print("LIDAR ready")
 
         # Begin the loop
         time_sim = 0
@@ -231,46 +251,58 @@ def main():
 
             # Check whether it's time to capture data
             if time_sim >= tick_sensor:
-                data = [retrieve_data(q,nowFrame) for q in q_list]
+                data = [retrieve_data(q, nowFrame) for q in q_list]
                 assert all(x.frame == nowFrame for x in data if x is not None)
 
                 # Skip if any sensor data is not available
                 if None in data:
                     continue
-                
-                vehicles_raw = world.get_actors().filter('vehicle.*')
+
+                vehicles_raw = world.get_actors().filter("vehicle.*")
                 snap = data[tick_idx]
                 rgb_img = data[cam_idx]
                 depth_img = data[depth_idx]
-                
+
                 # Attach additional information to the snapshot
                 vehicles = cva.snap_processing(vehicles_raw, snap)
 
                 # Save depth image, RGB image, and Bounding Boxes data
                 if save_depth:
-                    depth_img.save_to_disk('out_depth/%06d.png' % depth_img.frame, cc_depth_log)
+                    depth_img.save_to_disk(
+                        "out_depth/%06d.png" % depth_img.frame, cc_depth_log
+                    )
                 depth_meter = cva.extract_depth(depth_img)
-                filtered, removed =  cva.auto_annotate(vehicles, cam, depth_meter, json_path='vehicle_class_json_file.txt')
-                cva.save_output(rgb_img, filtered['bbox'], filtered['class'], removed['bbox'], removed['class'], save_patched=True, out_format='json')
-                
+                filtered, removed = cva.auto_annotate(
+                    vehicles, cam, depth_meter, json_path="vehicle_class_json_file.txt"
+                )
+                cva.save_output(
+                    rgb_img,
+                    filtered["bbox"],
+                    filtered["class"],
+                    removed["bbox"],
+                    removed["class"],
+                    save_patched=True,
+                    out_format="json",
+                )
+
                 # Uncomment if you want to save the data in darknet format
-                #cva.save2darknet(filtered['bbox'], filtered['class'], rgb_img)
+                cva.save2darknet(filtered["bbox"], filtered["class"], rgb_img)
 
                 # Save segmentation image
                 if save_segm:
                     segm_img = data[segm_idx]
-                    segm_img.save_to_disk('out_segm/%06d.png' % segm_img.frame, cc_segm)
+                    segm_img.save_to_disk("out_segm/%06d.png" % segm_img.frame, cc_segm)
 
                 # Save LIDAR data
                 if save_lidar:
                     lidar_data = data[lidar_idx]
-                    lidar_data.save_to_disk('out_lidar/%06d.ply' % segm_img.frame)
-                
+                    lidar_data.save_to_disk("out_lidar/%06d.ply" % segm_img.frame)
+
                 time_sim = 0
             time_sim = time_sim + settings.fixed_delta_seconds
 
     finally:
-        cva.save2darknet(None,None,None,save_train=True)
+        cva.save2darknet(None, None, None, save_train=True)
         try:
             cam.stop()
             depth.stop()
@@ -280,26 +312,25 @@ def main():
                 lidar.stop()
         except:
             print("Simulation ended before sensors have been created")
-        
+
         settings = world.get_settings()
         settings.synchronous_mode = False
         settings.fixed_delta_seconds = None
         world.apply_settings(settings)
 
-        print('\ndestroying %d vehicles' % len(vehicles_list))
+        print("\ndestroying %d vehicles" % len(vehicles_list))
         client.apply_batch([carla.command.DestroyActor(x) for x in vehicles_list])
 
-        print('destroying %d nonvehicles' % len(nonvehicles_list))
+        print("destroying %d nonvehicles" % len(nonvehicles_list))
         client.apply_batch([carla.command.DestroyActor(x) for x in nonvehicles_list])
 
         time.sleep(0.5)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
         pass
     finally:
-        print('\ndone.')
+        print("\ndone.")
